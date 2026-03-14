014188b77b8fb0dbd82abe377a786d7d39e095e6
Minh Bui <btminh.dev@gmail.com>
2026-03-14 15:23:58 +0700

    create notification comsumer to receiving noti from queue

---
+++ services/notification-service/server.js
@@ -0,0 +1,45 @@
+'use strict'
+
+require('dotenv').config()
+const { rabbitmq } = require('@shop/shared')
+
+const QUEUE_NAME = 'notification-queue'
+
+const runConsumer = async () => {
+  try {
+    const rabbitMQUrl = process.env.RABBITMQ_URI
+    await rabbitmq.connectRabbitMQ(rabbitMQUrl)
+    console.log('[Notification Service] Connected to RabbitMQ successfully')
+
+    await rabbitmq.consumeMessage(QUEUE_NAME, async (msgData) => {
+      console.log('----------------------------------------')
+      console.log(`[New Message] event: ${msgData.event}`)
+      console.log('Data:', msgData.data)
+      
+      switch (msgData.event) {
+        case 'PRODUCT_PUBLISHED':
+            // TODO: Handle the PRODUCT_PUBLISHED event
+            console.log(`>> Sending noti for new product: ${msgData.data.productName}`)
+            
+            // Giả lập thời gian gửi mail mất 2 giây
+            await new Promise(resolve => setTimeout(resolve, 2000))
+            console.log(`>> Sended noti successfully for product ${msgData.data.productId}!`)
+            break;
+            
+        case 'ORDER_SUCCESS':
+            break;
+
+        default:
+            console.log('>> Event not supported.')
+            break;
+      }
+      console.log('----------------------------------------')
+    })
+
+  } catch (error) {
+    console.error('Error in Notification Consumer:', error)
+    process.exit(1)
+  }
+}
+
+runConsumer()
